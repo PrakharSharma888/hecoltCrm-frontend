213958f45ebaaeed7f3dc4f161ea49472aa6e781
prakhar5 <prakhar@ith.tech>
2026-02-11 12:54:59 +0530

    Updated base url for apis

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "/api";
+const BASE_URL = "https://hecolt-crm-backend-git-main-prakharsharma888s-projects.vercel.app/api";
 
 const getHeaders = () => {
   const token = localStorage.getItem("token");
